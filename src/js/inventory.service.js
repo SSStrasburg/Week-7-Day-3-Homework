ca105432634913f8d50b6ee7be505e78cfcefe2d
Steven Strasburg <SSStrasburg@gmail.com>
2017-04-03 22:38:03 -0400

    trying to get the sort by name, quantity, and price to work in reverse and regular order

--- src/js/inventory.service.js
+++ src/js/inventory.service.js
@@ -8,22 +8,28 @@
     // JSON.parse from a string to an array
     let items = JSON.parse(localStorage.getItem('item')) || [];  // 'item' is the same as below
 
+    /**
+     * This function adds items to a database
+     * @param {Object} item with name, price, quantity, etc
+     */
     function addItem(item){
-      item.push = Number(item.price);
+      console.log('line 12 service');
+      item.price = Number(item.price);
       item.quantity = Number(item.quantity);
       item.discount = Number(item.discount);
 
       if (typeof(item) !== 'object' ||
           typeof(item.name) !== 'string' ||
-          item.name < 1 ||
+          item.name.length < 1 ||
           item.price < 0.01  ||
           typeof(item.price) !== 'number' ||
           Number.isNaN(item.price) ||
           typeof(item.quantity) !== 'number' ||
-          item.quantity >= 0 ||
+          item.quantity < 0 ||
           typeof(item.color) !== 'string' ||
+          item.color.length < 1 ||
           typeof(item.discount) !== 'number' ||
-          item.discount >= 0){
+          item.discount < 0){
         return;
       }
 
@@ -34,7 +40,7 @@
         color:  item.color,
         discount: item.discount
       });
-      localStorage.setItem('item', angular.toJson(inventory));  // 'item' is the same as above.
+      localStorage.setItem('item', angular.toJson(item));  // 'item' is the same as above.
       //we are stringafing
     }
 
